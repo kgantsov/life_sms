79fb2b83bf43e46809aefddd7d9c0445694f4f30
Konstantin Gantsov <k.gantsov@gmail.com>
2013-09-20 21:00:39 +0300

    Changed setup.py file

--- setup.py
+++ setup.py
@@ -2,7 +2,13 @@
 """ Setup file for Simimg package """
 
 
-from distutils.core import setup
+from setuptools import setup
+
+tests_require = [
+    'nose==1.2.1',
+    'mock==1.0.1',
+    'coveralls',
+]
 
 setup(
     name='life_sms',
@@ -13,13 +19,20 @@ setup(
     author_email='k.gantsov@gmail.com',
     packages=['life_sms'],
 
+    install_requires=[
+        "lxml==3.2.3",
+    ],
+    tests_require=tests_require,
+    test_suite="nose.collector",
+    extras_require={'test': tests_require},
     classifiers=(
         'Development Status :: 2 - Pre-Alpha',
         'Environment :: Console',
         'License :: OSI Approved :: GNU General Public License (GPL)',
-        'Operating System :: POSIX',
-        'Operating System :: Unix',
+        'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
     ),
     license="GPL-2"
 )
